9f93b87b5c17f3b42ec61b072bcdf7a003792f9a
abhayjohri23 <abhay.johri0@gmail.com>
2024-01-16 04:04:31 +0530

    Added a new migration file for the updated db. Previous flywayinitialzr was not working.

---
+++ src/test/java/com/springDataJPA/Assignment3/TestRandomFunctionality.java
@@ -0,0 +1,16 @@
+package com.springDataJPA.Assignment3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class TestRandomFunctionality {
+//    @Test
+//    public void testTheSummationOfTwoNumbers(){
+//        assert(true == true);
+//    }
+}
