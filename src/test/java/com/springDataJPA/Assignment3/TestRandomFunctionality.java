021e82f48e31f981711c689d277d16cf41dd2842
abhayjohri23 <abhay.johri0@gmail.com>
2024-01-16 04:19:31 +0530

    Added some very simple test cases - to understand the different functions and annotations in JUnit5.

--- src/test/java/com/springDataJPA/Assignment3/TestRandomFunctionality.java
+++ src/test/java/com/springDataJPA/Assignment3/TestRandomFunctionality.java
@@ -4,13 +4,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 @SpringBootTest
 public class TestRandomFunctionality {
-//    @Test
-//    public void testTheSummationOfTwoNumbers(){
-//        assert(true == true);
-//    }
+    @Test
+    public void testTheSummationOfTwoNumbers(){
+        assertTrue(59+42 == 101);
+    }
+
+    @Test
+    public void testAssertAllAnotation(){
+        int[] arr1 = {-1,-2,-3};
+        int[] arr2 = {-1,-2,-3};
+
+        assertAll("Assert all working for below positive scenarios",
+                () -> assertEquals(0,1+(-1)),
+                () -> assertTrue(true ==true),
+                () -> assertEquals("Hey, I am a test","Hey, I am a test"),
+                () -> assertNotEquals("===","equals()"),
+                () -> assertArrayEquals(arr1,arr2));
+    }
+
+    @Test
+    @DisplayName("AssumptionScenario")
+    public void testAssumptionsWithAssertions(){
+        assumeFalse(2 == 7);
+        assertFalse(7+7 == 9);
+    }
 }
